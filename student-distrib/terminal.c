6430e1e4f8ea8d02b423a6cd38944e9cd97f5fa0
Kyle Tolentino <kyltol@gmail.com>
2020-10-22 01:57:51 -0500

    added terminal.h, terminal.c

---
+++ student-distrib/terminal.c
@@ -0,0 +1,43 @@
+/* terminal.c - Functions for terminal driver
+ */
+
+#include "terminal.h"
+
+/* terminal_open(const uint8_t* command)
+ * Inputs: filename - not relevant to terminal
+ * Return Value: int32_t (0 on success, -1 on failure)
+ * Function: not relevant to terminal */
+int32_t terminal_open(const uint8_t* filename){
+    return 0;
+}
+
+
+/* terminal_close(int32_t fd)
+ * Inputs: fd - not relevant to terminal
+ * Return Value: int32_t (0 on success, -1 on failure)
+ * Function: not relevant to terminal */
+int32_t terminal_close(int32_t fd){
+    return 0;
+}
+
+
+/* terminal_read(int32_t fd, void* buf, int32_t nbytes)
+ * Inputs: fd - not relevant to terminal
+ *          buf - data from keyboard
+ *          nbytes - number of bytes to read
+ * Return Value: int32_t (number of bytes written on success, -1 on failure?)
+ * Function: not relevant to terminal */
+int32_t terminal_read(int32_t fd, void* buf, int32_t nbytes){
+    
+
+    clear_keyboard_buf();       // need to clear the buffer at the end
+}
+
+
+/* terminal_write(int32_t fd, const void* buf, int32_t nbytes)
+ * Inputs: fd - not relevant to terminal
+ *          buf - data to write
+ * Return Value: int32_t (number of bytes written on success, -1 on failure)
+ * Function: not relevant to terminal */
+int32_t terminal_write(int32_t fd, const void* buf, int32_t nbytes){}
+
