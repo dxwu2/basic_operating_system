1f790b4ee36532c262493021e054882fe5f3649c
David Antonowicz <dopalini@gmail.com>
2020-10-26 01:12:01 -0400

    Added fix to allow for fname>32 when reading dentry, fixed extra char in file_list print, finished comments and headers

--- student-distrib/tests.c
+++ student-distrib/tests.c
@@ -130,11 +130,11 @@ int fs_test_1(){
 	TEST_HEADER;
 
 	dentry_t test_dentry;
-	read_dentry_by_name("frame0.txt", &test_dentry);
-	if(strncmp(test_dentry.filename, "frame0.txt", 10) != 0)
+	read_dentry_by_name("verylargetextwithverylongname.txt", &test_dentry);
+	if(strncmp(test_dentry.filename, "verylargetextwithverylongname.tx", 32) != 0)
 		return FAIL;
-	/* "frame0.txt" should correspond to inode 38 and filetype 2*/
-	if(test_dentry.inode_num != 38 || test_dentry.filetype != 2)
+	/* "verylargetextwithverylongname.tx" should correspond to inode 44, filetype 2*/
+	if(test_dentry.inode_num != 44 || test_dentry.filetype != 2)
 		return FAIL;	
 	else
 		return PASS;
@@ -155,6 +155,7 @@ int fs_test_2(){
 	read_dentry_by_index(7, &test_dentry);
 	if(strncmp(test_dentry.filename, "counter", 7) != 0)
 		return FAIL;
+	/* "counter" should correspond to inode 42, filetype 2 */
 	if(test_dentry.inode_num != 42 || test_dentry.filetype != 2)
 		return FAIL;
 	else
@@ -173,12 +174,20 @@ void fs_test_list_files(){
 	TEST_HEADER;
 
 	dentry_t cur_dentry;
-	int i;
+	int i, j;
 	for (i = 0; i < 17; i++) {
 		dir_read(i, &cur_dentry, 64);
+
+		/* Create name buffer to copy fname into + one extra space for null '\0' char */
+		uint8_t fname[FILENAME_LEN + 1];
+		for(j = 0; j < FILENAME_LEN; j++){
+			fname[j] = cur_dentry.filename[j];
+		}
+		fname[32] = '\0';	//terminate any string over 32 chars with null char
+
 		uint32_t inode_idx = cur_dentry.inode_num;
-		inode_t* inode_addr = filesystem_start + (4096 * (inode_idx+1)); //calculate inode struct addr using inode number and start addr of filesystem
-		printf("file_name: %s, file_type: %d, file_size: %d\n", cur_dentry.filename, cur_dentry.filetype, inode_addr->length);
+		inode_t* inode_addr = (inode_t*) (filesystem_start + (4096 * (inode_idx+1))); //calculate inode struct addr using inode number and start addr of filesystem
+		printf("file_name: %s, file_type: %d, file_size: %d\n", fname, cur_dentry.filetype, inode_addr->length);
 	}
 	
 }
@@ -224,6 +233,6 @@ void launch_tests(){
 	// null_test();
 	// TEST_OUTPUT("fs_test_1", fs_test_1());
 	// TEST_OUTPUT("fs_test_2", fs_test_2());
-	fs_test_list_files();
+	// fs_test_list_files();
 	// fs_test_read_file();
 }
