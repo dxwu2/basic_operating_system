0141c1a064cf8be3e42c05f22a5627a6c6710542
David Antonowicz <dopalini@gmail.com>
2020-10-28 16:26:08 -0400

    Started system call wrapper and declared individual calls

--- student-distrib/syscall.c
+++ student-distrib/syscall.c
@@ -11,3 +11,19 @@ void handle_system_call(){
     printf("System call (vector 0x80) was called.\n");
     while(1);
 }
+
+int32_t sys_halt (uint8_t status){}
+
+int32_t sys_execute (const uint8_t command){}
+
+int32_t sys_read (int32_t fd, void* buf, int32_t nbytes){}
+
+int32_t sys_write (int32_t fd, void* buf, int32_t nbytes){}
+
+int32_t sys_open (const uint8_t* filename){}
+
+int32_t sys_close (int32_t fd){}
+
+int32_t sys_getargs (uint8_t* buf, int32_t nbytes){}
+
+int32_t sys_vidmap (uint8_t** screen_start){}
--- student-distrib/syscall.h
+++ student-distrib/syscall.h
@@ -6,4 +6,14 @@
 /* System call handler declaration */
 void system_call();
 
+/* Actual system call handlers (CP3) */
+int32_t sys_halt (uint8_t status);
+int32_t sys_execute (const uint8_t command);
+int32_t sys_read (int32_t fd, void* buf, int32_t nbytes);
+int32_t sys_write (int32_t fd, void* buf, int32_t nbytes);
+int32_t sys_open (const uint8_t* filename);
+int32_t sys_close (int32_t fd);
+int32_t sys_getargs (uint8_t* buf, int32_t nbytes);
+int32_t sys_vidmap (uint8_t** screen_start);
+
 #endif
