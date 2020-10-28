357a30a4b03f4d4ee9a4e2c53de166ede225aef5
David Antonowicz <dopalini@gmail.com>
2020-10-28 16:35:13 -0400

    keep comitting before checking for compiler errors dammit

--- student-distrib/syscall.c
+++ student-distrib/syscall.c
@@ -12,18 +12,34 @@ void handle_system_call(){
     while(1);
 }
 
-int32_t sys_halt (uint8_t status){}
+int32_t sys_halt (uint8_t status){
+    return 0;
+}
 
-int32_t sys_execute (const uint8_t command){}
+int32_t sys_execute (const uint8_t command){
+    return 0;
+}
 
-int32_t sys_read (int32_t fd, void* buf, int32_t nbytes){}
+int32_t sys_read (int32_t fd, void* buf, int32_t nbytes){
+    return 0;
+}
 
-int32_t sys_write (int32_t fd, void* buf, int32_t nbytes){}
+int32_t sys_write (int32_t fd, void* buf, int32_t nbytes){
+    return 0;
+}
 
-int32_t sys_open (const uint8_t* filename){}
+int32_t sys_open (const uint8_t* filename){
+    return 0;
+}
 
-int32_t sys_close (int32_t fd){}
+int32_t sys_close (int32_t fd){
+    return 0;
+}
 
-int32_t sys_getargs (uint8_t* buf, int32_t nbytes){}
+int32_t sys_getargs (uint8_t* buf, int32_t nbytes){
+    return 0;
+}
 
-int32_t sys_vidmap (uint8_t** screen_start){}
+int32_t sys_vidmap (uint8_t** screen_start){
+    return 0;
+}
