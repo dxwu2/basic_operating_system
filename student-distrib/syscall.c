d0a265102c765709930a1a149f53f78456bcc557
Kyle Tolentino <kyltol@gmail.com>
2020-11-09 15:57:52 -0600

    we done

--- student-distrib/syscall.c
+++ student-distrib/syscall.c
@@ -555,6 +555,11 @@ pcb_t* get_pcb_ptr(void){
 }
 
 /* pcb_t - write a get pcb from pid function. also, when setting flags in line 444, not the same as the pcb addr in sys write */
+/* get_pcb_from_pid(int pid)
+ * Gets a pointer to the pcb corresponding to a pid
+ * Inputs   : pid
+ * Outputs  : a pcb pointer
+ */
 pcb_t* get_pcb_from_pid(int pid) {
     pcb_t* pcb_ptr;
     pcb_ptr = (pcb_t*)(EIGHT_MB - (pid + 1) * EIGHT_KB);
